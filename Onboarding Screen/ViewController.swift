2d30f0c27cd77b2f38ac1942635c25ddee358385
benjopw <benyaminpurbowasito@gmail.com>
2019-07-20 12:43:34 +0700

    Initial Commit

---
+++ Onboarding Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Onboarding Screen
+//
+//  Created by Benjamin Purbowasito on 20/07/19.
+//  Copyright © 2019 iosda Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
