f16e27667b1400bb5400483df16a23d8b2f106b6
benjopw <benyaminpurbowasito@gmail.com>
2019-07-21 17:12:13 +0700

    udah import asset tap belom jalan paka code, atau cuman segue doang?

---
+++ Onboarding Screen/PageControlViewController.swift
@@ -0,0 +1,99 @@
+//
+//  PageControlViewController.swift
+//  Onboarding Screen
+//
+//  Created by Benjamin Purbowasito on 20/07/19.
+//  Copyright © 2019 iosda Ben. All rights reserved.
+//
+
+import UIKit
+
+class PageControlViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate{
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVC(viewController: "screenOne"), self.newVC(viewController: "screenTwo"), self.newVC(viewController: "screenThree")]
+    }()
+    
+    var pageControl = UIPageControl()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+        self.delegate = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        
+        configurePageControl()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.firstIndex(of: pageContentViewController)!
+    }
+    
+    func configurePageControl() {
+        pageControl = UIPageControl (frame: CGRect(x:0, y: UIScreen.main.bounds.maxY - 50, width: UIScreen.main.bounds.width, height: 50))
+        pageControl.numberOfPages = orderedViewControllers.count
+        pageControl.currentPage = 0
+        pageControl.tintColor = .white
+        pageControl.currentPageIndicatorTintColor = .orange
+        self.view.addSubview(pageControl)
+        
+    }
+    
+    func newVC(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex =  orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex =  orderedViewControllers.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard orderedViewControllers.count > nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
